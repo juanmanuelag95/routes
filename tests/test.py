a1d86535c30b7378ece720ccce61681c34e4a4b5
Juan Alvarez <juanmanuelag95@gmai.com>
2021-09-16 16:25:28 -0500

    test_a completed

--- tests/test.py
+++ tests/test.py
@@ -5,9 +5,26 @@ import pytest
 def baseURL():
     return "http://localhost:3000"
 
-def test_get(baseURL):
+def test_a(baseURL):
+    plannedDeliveries = []
+
+    url = baseURL + "/deliveries_for_planning?current_state=planned"
+    response = requests.get(url)
+    assert response.status_code == 200
+
+    for delivery in response.json():
+        plannedDeliveries.append(delivery["id"])
 
     url = baseURL + "/planned_route"
     response = requests.get(url)
     assert response.status_code == 200
 
+    totalDeliveries = response.json()["deliveries"]
+    founds = 0
+
+    for delivery in totalDeliveries:
+        if delivery["id"] in plannedDeliveries:
+            founds += 1
+
+    assert founds == len(plannedDeliveries)
+    
